3c8d4591c66ab244c4b7dce875eb2ea5895ba254
Abhishek Bhatia <abhibhatia0410@gmail.com>
2026-09-11 15:41:40 +0530

    fix: update API base URL and correct syntax in admin seed script

--- server/adminSeed.js
+++ server/adminSeed.js
@@ -9,7 +9,7 @@ const seedAdmin = async () => {
     console.log("Database connected.");
 
     const existingAdmin = await User.findOne({
-      where: {d
+      where: {
         email: "admin@aistudentassistant.com",
       },
     });
